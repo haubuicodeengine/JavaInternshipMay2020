db3785c5811845e0dee7406babea457a556a8bc0
thuyct99 <thuycao625@gmail.com>
2020-05-22 08:39:42 +0700

    Exercise1

---
+++ src/codeenginestudio/thuy/Constants.java
@@ -0,0 +1,9 @@
+package codeenginestudio.thuy;
+
+public class Constants {
+    public static final int DEFAULT_NUMBER = 0;
+
+    public static final String SPACE = " ";
+
+    public static final String SPLIT_PATTERN = "\\s";
+}
